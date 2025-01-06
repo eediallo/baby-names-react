15377922ded8df54847fde085186bb0e29976add
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-06 19:21:20 +0000

    add style based on sex

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,28 @@ import "./App.css";
 import { babyNames } from "../data/babyNames";
 
 function App() {
-  const babyNameList = babyNames.map((babyName) => (
-    <p className="name" key={babyName.id}>{babyName.name}</p>
-  ));
+  const babyNameList = babyNames.map((babyName) => {
+    if (babyName.sex === "f") {
+      return (
+        <p
+          className="female"
+          key={babyName.id}
+          style={{ backgroundColor: "pink", color: "red" }}
+        >
+          {babyName.name}
+        </p>
+      );
+    }
+    return (
+      <p
+        className="male"
+        key={babyName.id}
+        style={{ backgroundColor: "blue", color: "white" }}
+      >
+        {babyName.name}
+      </p>
+    );
+  });
 
   return <>{babyNameList}</>;
 }
