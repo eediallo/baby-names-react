7e947815bfd8a5ef2e04b80cd323ba426924a761
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-06 10:15:20 +0000

    add class to name

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { babyNames } from "../data/babyNames";
 
 function App() {
   const babyNameList = babyNames.map((babyName) => (
-    <p key={babyName.id}>{babyName.name}</p>
+    <p className="name" key={babyName.id}>{babyName.name}</p>
   ));
 
   return <>{babyNameList}</>;
