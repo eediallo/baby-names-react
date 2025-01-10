6d151ade0d2887c83d4da6c96818678db8c8c189
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-10 17:17:20 +0000

    move favorites back to names on onclick and set useEffect

---
+++ components/renderBabyNames.jsx
@@ -0,0 +1,57 @@
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+
+export function BabyNames({ babyNames }) {
+  let [favorites, setFavorites] = useState([]);
+  let [names, setNames] = useState(babyNames);
+
+  function handleMoveToFavoritesClick(babyName) {
+    setNames(names.filter((name) => name !== babyName));
+    setFavorites((prvFavorites) => [...prvFavorites, babyName]);
+  }
+
+  function handleMoveToNamesClick(name) {
+    setFavorites(favorites.filter((favoriteName) => favoriteName !== name));
+    setNames((prvNames) => [...prvNames, name]);
+  }
+
+  useEffect(() => {
+    setNames(babyNames);
+  }, [babyNames]);
+
+  const namesList = names.map((babyName) => {
+    return (
+      <div key={babyName.id}>
+        <button onClick={() => handleMoveToFavoritesClick(babyName)}>
+          <p className={babyName.sex === "f" ? "female name" : "male name"}>
+            {babyName.name}
+          </p>
+        </button>
+      </div>
+    );
+  });
+
+  const favoritesList = favorites.map((favorite) => {
+    return (
+      <div key={favorite.name}>
+        <button onClick={() => handleMoveToNamesClick(favorite)}>
+          <p>{favorite.name}</p>
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div>
+        <h1>Favorites:</h1>
+        <div className="favorites-container">{favoritesList}</div>
+      </div>
+      <div className="names-container">{namesList}</div>
+    </div>
+  );
+}
+
+BabyNames.propTypes = {
+  babyNames: PropTypes.array.isRequired,
+};
