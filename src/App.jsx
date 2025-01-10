db4d18c4f59567910a42f81f687d18d697ceb94b
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-10 13:35:34 +0000

    refactor to handle search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
 import { BabyNames } from "../ components/renderBabyNames.jsx";
-import { SearchBar } from "../ components/searchBar.jsx";
 import { babyNames } from "../data/babyNames.jsx";
+import { useState } from "react";
 
 import "./App.css";
 
 function App() {
+  let [searchItem, setSearchItem] = useState("");
+  let [filteredBabyNames, setfilteredBabyNames] = useState(babyNames);
+
+  function handleOnInput(e) {
+    searchItem = e.target.value;
+    setSearchItem(searchItem);
+
+    filteredBabyNames = babyNames.filter((babyName) => {
+      const name = babyName.name.toLowerCase();
+      return name.includes(searchItem);
+    });
+
+    setfilteredBabyNames(filteredBabyNames);
+  }
+
   return (
     <>
       <h1>Baby Name Picker</h1>
-      <SearchBar />
+      <label htmlFor="seach-bar"></label>
+
+      <div className="search-bar-container">
+        <input
+          type="text"
+          id="search-bar"
+          value={searchItem}
+          onChange={(e) => setSearchItem(e.target.value)}
+          onInput={handleOnInput}
+          placeholder="Search for a name"
+        />
+      </div>
+
       <div className="names-container">
-        <BabyNames babyNames={babyNames} />
+        <BabyNames babyNames={filteredBabyNames} />
       </div>
     </>
   );
