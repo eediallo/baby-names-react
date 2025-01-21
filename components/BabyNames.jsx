76fec7706d1d6a78e2a85997f58ab69ca8997a8b
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-21 22:40:20 +0000

    refactor --> moves namesList to its own component

---
+++ components/BabyNames.jsx
@@ -0,0 +1,25 @@
+import BabyName from "./BabyName";
+import PropTypes from "prop-types";
+
+export default function BabyNames({ names, setNames }) {
+  function handleMoveToFavoritesClick(babyName) {
+    setNames(names.filter((name) => name !== babyName));
+  }
+
+  return (
+    <div className="names-container">
+      {names.map((babyName) => (
+        <BabyName
+          onSelect={() => handleMoveToFavoritesClick(babyName)}
+          key={babyName.id}
+          {...babyName}
+        />
+      ))}
+    </div>
+  );
+}
+
+BabyNames.propTypes = {
+  names: PropTypes.array.isRequired,
+  setNames: PropTypes.func.isRequired,
+};
