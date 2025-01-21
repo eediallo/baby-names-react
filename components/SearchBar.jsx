2992b6b72c4d07f2f035ab97cfe3df30c1f87d05
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-21 09:29:29 +0000

    refactor -> create Search Bar component

---
+++ components/SearchBar.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { babyNames } from "../data/babyNames.jsx";
+import PropTypes from "prop-types";
+
+export default function SearchBar({ setfilteredBabyNames }) {
+  let [searchItem, setSearchItem] = useState("");
+  function handleInput(e) {
+    const searchValue = e.target.value.toLowerCase();
+    setSearchItem(searchValue);
+
+    const filteredNames = babyNames.filter((babyName) => {
+      const name = babyName.name.toLowerCase();
+      return name.includes(searchValue);
+    });
+
+    setfilteredBabyNames(filteredNames);
+  }
+  return (
+    <div className="search-bar-container">
+      <input
+        type="text"
+        id="search-bar"
+        value={searchItem}
+        onChange={handleInput}
+        placeholder="Search for a name"
+      />
+    </div>
+  );
+}
+
+SearchBar.propTypes = {
+  setfilteredBabyNames: PropTypes.func.isRequired,
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,19 @@
 import { BabyNames } from "../components/renderBabyNames.jsx";
+import SearchBar from "../components/SearchBar.jsx";
 import { babyNames } from "../data/babyNames.jsx";
 import { useState } from "react";
-
 import "./App.css";
 
 function App() {
-  let [searchItem, setSearchItem] = useState("");
-  // sort in ascending order initially
   let [filteredBabyNames, setfilteredBabyNames] = useState(
     babyNames.sort((a, b) => a.name.localeCompare(b.name))
   );
-
-  function handleOnInput(e) {
-    const searchValue = e.target.value.toLowerCase();
-    setSearchItem(searchValue);
-
-    const filteredNames = babyNames.filter((babyName) => {
-      const name = babyName.name.toLowerCase();
-      return name.includes(searchValue);
-    });
-
-    setfilteredBabyNames(filteredNames);
-  }
 
   return (
     <>
       <h1>Baby Name Picker</h1>
       <label htmlFor="seach-bar"></label>
-
-      <div className="search-bar-container">
-        <input
-          type="text"
-          id="search-bar"
-          value={searchItem}
-          onChange={handleOnInput}
-          placeholder="Search for a name"
-        />
-      </div>
+      <SearchBar setfilteredBabyNames={setfilteredBabyNames} />
       <BabyNames babyNames={filteredBabyNames} />
     </>
   );
